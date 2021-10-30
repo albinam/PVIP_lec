a6a89eabb4465ba42de4932d91ca9849193dad6a
albinosikm <albinosikm@gmail.com>
2021-10-30 20:23:51 +0400

    пример с заданным временем

--- PVIP_lec/Program.cs
+++ PVIP_lec/Program.cs
@@ -10,9 +10,9 @@ namespace PVIP_lec
             Lamport lamport = new Lamport();
 
             Event e1 = new Event(1, "Первое событие", null);
-            Event e2 = new Event(2, "Второе событие", null);
+            Event e2 = new Event(2, "Второе событие", 6);
             Event e3 = new Event(3, "Третье событие", null);
-            Event e4 = new Event(4, "Четвертое событие", null);
+            Event e4 = new Event(4, "Четвертое событие", 9);
             Event e5 = new Event(5, "Пятое событие", null);
 
             lamport.receive(e1);
